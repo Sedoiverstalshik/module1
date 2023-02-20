ddc633f35198b7864935869fef251626af0797f2
Николай Асташев <sedoiverstalshik@mail.ru>
2023-02-20 19:49:11 +0300

    исправил 2 задание

--- Task02.js
+++ Task02.js
@@ -3,11 +3,9 @@
 const isPrime = (num) => {
 
   for(let i = 2; i < num; i++){
-    if(num % i === 0 && num > 1) {
+    if(num % i === 0) {
       return false;
     }
-    return true;
   }
+  return num > 1;
 }
-
-console.log(isPrime(12));
