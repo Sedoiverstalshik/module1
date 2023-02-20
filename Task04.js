3a94a565545b47687d0a1a729cb4b2091abb61fc
Николай Асташев <sedoiverstalshik@mail.ru>
2023-02-20 19:16:51 +0300

    исправил 4 задание

--- Task04.js
+++ Task04.js
@@ -2,31 +2,26 @@
 
 const calculate = (sumCart, count, promo = null) => {
 
-  let price;
+  let price = sumCart;
 
-  switch (true) {
-
-    case (count > 10):
-      price = sumCart - (sumCart * 0.03);
-      console.log('price1: ', price);
-      return;
-
-    case (sumCart >= 30000):
-      price = sumCart - (sumCart - 30000) * 0.15;
-      console.log('price2: ', price);
-      return;
-
-    case (promo === "METHED"):
-      price = sumCart - sumCart * 0.2;
-      console.log('price3: ', price);
-      return;
-
-    case( promo === "G3H2Z1" && sumCart >= 2000):
-      price = sumCart - 500;
-      console.log('price4: ', price);
-      return;
+  if(count > 10) {
+    price *= 0.97;
   }
 
+  if(sumCart > 30000) {
+    price = (price - 30000) * 0.85 + 30000;
+  }
+
+  if(promo === "METHED") {
+    price *= 0.9;
+  }
+
+  if(promo === "G3H2Z1" && sumCart > 2000) {
+    price -= 500;
+  }
+
+  return price;
 }
-const res = calculate(42000, 2, );
-console.log('res: ', res);
+  const res = calculate(100000, 20, "METHED");
+  console.log('res: ', res);
+
