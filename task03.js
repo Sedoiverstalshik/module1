e354ff2d377002993cadd3fde3dbd4b1b41d1cd4
Николай Асташев <sedoiverstalshik@mail.ru>
2023-03-02 20:39:32 +0300

    исправил 3 задание

--- task03.js
+++ task03.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+{
+  const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
 
 const addPrefix = (array, pref) => {
 
+  const prefix = [];
+
   for(const elem of array ) {           //перебираем массив и добавляем префикс
-    console.log(`${pref} ${elem}`);
+    prefix.push(`${pref} ${elem}`);
   }
+  return prefix;
 }
 
-addPrefix(names, 'Mr');
+const result = addPrefix(names, 'Mr');
+console.log('result: ', result);
+}
