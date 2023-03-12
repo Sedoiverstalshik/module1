80dc94901a8f00917480ccfbdf6d76412e3dee16
Николай Асташев <sedoiverstalshik@mail.ru>
2023-03-12 21:39:00 +0300

    исправил домашнее задание

--- cart.js
+++ cart.js
@@ -12,41 +12,36 @@ const cart = {
   add(productName, productPrice, productCount) {
     cart.items.push({productName, productPrice, productCount});
     cart.increaseCount(productCount);
-    cart.calculateItemPrice(productPrice, productCount);
-    cart.getTotalPrice();
+    cart.calculateItemPrice();
   },
 
   increaseCount(num) {
-    return cart.count += num;
+    cart.count += num;
   },
   
-  calculateItemPrice(price, count) {
-    cart.totalPrice = (price * count) +  cart.totalPrice;
-    return cart.totalPrice; 
-  },
-  
-  //возникли проблемы с очисткой корзины
-
+  calculateItemPrice() {
+    cart.totalPrice = cart.items.reduce((acc, item) => {
+      return acc + item.productPrice * item.productCount;
+    }, 0);
+  },  
   
   clear() {
     cart.items = [];
     cart.totalPrice = 0;
     cart.count = 0;
-    cart.add();
   },
-
+  
   print() {
     console.log(JSON.stringify(cart.items));
     console.log(`Общая стоимость корзины ${cart.totalPrice}`);
   },
 };
 
-// cart.clear();
-cart.add('соль', 40, 2);
+cart.add('соль', 40, 1);
 cart.add('сахар', 60, 2);
 cart.add('молоко', 40, 1);
-cart.add('сметана', 50, 3);
 // console.log('items: ', cart.items);
 // console.log('totalPrice: ', cart.totalPrice);
 // console.log('count: ', cart.count);
 cart.print();
+
