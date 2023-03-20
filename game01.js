fc51c5e92bf1b014443afc21629585c9fc1d52a6
Николай Асташев <sedoiverstalshik@mail.ru>
2023-03-20 21:54:54 +0300

    выполнил 1 задание

--- game01.js
+++ game01.js
@@ -4,29 +4,57 @@ let number = '';
 const botNumber = Math.ceil(Math.random() * 100);
 console.log('botNumber: ', botNumber);
 
-while (number !== null && +number !== 100) {
-  number = prompt('Введите число от 0 до 100')
-  switch (true) {
-    case number === null:
-      alert('Игра окончена!');
-      break;
-
-    case isNaN(number):
-    case +number < 1:
-    case +number > 100:
-      alert('Вы ввели не число');
-      break;
-
-    case +number > botNumber:
-      alert('Меньше!');
-      break;
-
-    case +number < botNumber:
-      alert('Больше!');
-      break;
-
-    default:
-      alert('Правильно!');
+const game = () => {
+  if (number !== null && +number !== 100) {
+    number = prompt('Введите число от 0 до 100');
   }
-}
+  if (isNaN(number) || +number < 1 || +number > 100) {
+    alert('Вы ввели не число');
+    game();
+  }
+  if (number < botNumber) {
+    pluse(number);
+  }
+  if (number > botNumber) {
+    minus(number);
+  } else {
+    alert('Правильно!');
+  }
+};
+
+const pluse = (number) => {
+  const pluse = alert('Больше!');
+  game(number = pluse);
+};
+
+const minus = (number) => {
+  const minus = alert('Меньше!');
+  game(number = minus);
+};
+game();
+// while (number !== null && +number !== 100) {
+//   number = prompt('Введите число от 0 до 100')
+//   switch (true) {
+//     case number === null:
+//       alert('Игра окончена!');
+//       break;
+
+//     case isNaN(number):
+//     case +number < 1:
+//     case +number > 100:
+//       alert('Вы ввели не число');
+//       break;
+
+//     case +number > botNumber:
+//       alert('Меньше!');
+//       break;
+
+//     case +number < botNumber:
+//       alert('Больше!');
+//       break;
+
+//     default:
+//       alert('Правильно!');
+//   }
+// }
 
