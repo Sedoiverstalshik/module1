a8d5d8b969f402f07ce439cd9740fb2e8df40f76
Николай Асташев <sedoiverstalshik@mail.ru>
2023-03-29 20:22:21 +0300

    e

--- game01.js
+++ game01.js
@@ -32,33 +32,50 @@ const game = () => {
 // 2 задание
 
 {
-  const arrN = [2, 6, 3, 5, 6, 7, 3, 1];
+  const arrN = [2, 6, 3, 7, 3, 7, 5, 4, 10, 5, 7];
   console.log('arrN: ', arrN);
 
   let sum = 0;
   const randomArray = (array) => {
     let arrayNumbers = [];
-    const randomNumber = Math.floor(Math.random() * 11);
-    console.log('randomNumber: ', randomNumber);
-    // arrayNumbers.push(randomNumber);
-    // arrayNumbers = array;
-    sum += randomNumber;
-    console.log('sum: ', sum);
     if (array) {
       arrayNumbers = array.map((item) => sum += item);
-      console.log('sum2: ', sum);
-      // arrayNumbers.push();
+      console.log('arrayNumbers1: ', arrayNumbers);
+      console.log('sum1: ', sum);
     }
-    console.log('arrayNumbers1: ', arrayNumbers);
     if (sum > 50) {
       return arrayNumbers;
-    } else {
-      // arrayNumbers.push(array);
+    }
+    const randomNumber = Math.floor(Math.random() * 11);
+    console.log('randomNumber: ', randomNumber);
+    arrayNumbers.push(randomNumber);
+    sum += randomNumber;
+    console.log('sum2: ', sum);
+    if (sum < 50) {
+      console.log('arrayNumbers3: ', arrayNumbers);
       randomArray();
     }
-    console.log('arrayNumbers: ', arrayNumbers);
+    console.log('arrayNumbers4: ', arrayNumbers);
   };
 
-  randomArray(arrN);
+  randomArray();
 }
 
+
+// let arrayNumbers = [];
+//     const randomNumber = Math.floor(Math.random() * 11);
+//     console.log('randomNumber: ', randomNumber);
+//     switch (true) {
+//       case (array && sum > 50):
+//         arrayNumbers = array.map((item) => sum += item);
+//         console.log('arrayNumbers1: ', arrayNumbers);
+//         return arrayNumbers;
+//       case (sum < 50):
+//         arrayNumbers.push(randomNumber);
+//         sum += randomNumber;
+//         console.log('sum: ', sum);
+//         console.log('arrayNumbers2: ', arrayNumbers);
+//         randomArray();
+//       case (sum > 50):
+//         return arrayNumbers;
+//     }
